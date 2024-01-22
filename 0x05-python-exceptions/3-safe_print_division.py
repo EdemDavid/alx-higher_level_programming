c64fa7968c549cea38cfc35664fee42b7d072e1d
EdemDavid <daviddufe9@gmail.com>
2024-01-22 11:46:06 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        all_in_together_now = a/b
+        all_in_together_now = a / b
     except (TypeError, ZeroDivisionError):
         all_in_together_now = None
     finally:
